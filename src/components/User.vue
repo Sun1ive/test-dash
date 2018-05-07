f2292c40c994c3ef95178402a066e8535d182af8
Sun1ive <sunliveua@gmail.com>
2018-05-07 16:35:28 +0300

    gettin better

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,25 +1,61 @@
 <template>
   <div class="mycard">
     <div class="avatar">
-      <img src="https://randomuser.me/api/portraits/men/69.jpg">
+      <img :src="photo">
     </div>
-    <div class="box__wrapper">Alexander Myzuka<span class="box__wrapper__text">text</span></div>
-    <div class="box__wrapper">Alexander Myzuka<span class="box__wrapper__text">text</span></div>
-    <div class="box__wrapper value">112<span class="box__wrapper__text">text</span></div>
+    <div
+      class="box__wrapper">{{ name }}<span class="box__wrapper__text">Name</span></div>
+    <div
+      class="box__wrapper">{{ manager }}<span class="box__wrapper__text">Manager</span></div>
+    <div
+      :class="setClass(value)"
+      class="box__wrapper value">{{ value }}<span class="box__wrapper__text">Value</span></div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  props: {
+    percent: {
+      type: Number,
+      default: () => 100,
+    },
+    photo: {
+      type: String,
+      default: () => 'https://randomuser.me/api/portraits/men/69.jpg',
+    },
+    name: {
+      type: String,
+      default: () => 'Alexander Myzuka',
+    },
+    manager: {
+      type: String,
+      default: () => 'Alexander Myzuka',
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    setClass(value: number) {
+      if (value > 100) {
+        return 'blue';
+      } else if (value > this.percent) {
+        return 'green';
+      }
+      return 'red';
+    },
+  },
+});
 </script>
 
 <style scoped lang="stylus">
 .mycard
   max-width 1064px
   padding 1rem 0
-  margin auto
   font-family: 'Arvo', serif;
   border-radius 11px
   box-shadow 0px 2px 10px rgba(green, .4)
@@ -31,6 +67,7 @@ export default Vue.extend({});
   align-items center
   justify-content space-around
   font-size 2rem
+  margin 1rem auto
   .value
     border-radius 31px
     border 1px solid #eee
@@ -49,5 +86,5 @@ export default Vue.extend({});
   .green
     background-color green + 50%
   .blue
-    background-color blue + 50%
+    background-color blue + 80%
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,14 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <User />
+    <v-layout column>
+      <v-flex v-for="user in Users" :key="user.name">
+        <User
+          :name="user.name"
+          :manager="user.manager"
+          :photo="user.photo"
+          :percent="33"
+          :value="user[category]"
+        />
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,8 +17,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import User from '@/components/User.vue';
+import Users from '@/db/Users.json';
 
 export default Vue.extend({
   components: { User },
+  data: () => ({
+    Users,
+    category: 'perne',
+  }),
 });
 </script>
